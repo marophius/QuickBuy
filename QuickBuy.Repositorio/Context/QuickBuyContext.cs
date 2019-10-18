982161a0c4e8d8b945a7646e00211cb0501e7297
Icaro Félix <ifeme08@gmail.com>
2019-10-18 15:37:59 -0300

    Implementação do Entity Framework e das strings de conexão

---
+++ QuickBuy.Repositorio/Context/QuickBuyContext.cs
@@ -0,0 +1,24 @@
+﻿
+
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entities;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Repositorio.Context
+{
+    public class QuickBuyContext : DbContext
+    {
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Request> Request { get; set; }
+        public DbSet<OrderedItem> OrderedItem { get; set; }
+        public DbSet<PaymentForm> PaymentForm { get; set; }
+
+        public QuickBuyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
